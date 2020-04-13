9bdceb0f73e8c50deb61452e05447582565bea55
amiremohamadi <amiremohamadi@yahoo.com>
2020-04-13 13:26:11 +0430

    render static index.html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
-    return 'this is gonna be our home page'
+    return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
